3869bb3c3b84fdba2df7da012648e9944807b265
Vishal <vishalsidhu12@gmail.com>
2026-09-10 00:06:58 +0530

    Secure auth cookie

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -13,7 +13,7 @@ module.exports.Signup = async (req, res, next) => {
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
        sameSite: "none"
     });
@@ -43,9 +43,9 @@ module.exports.Login = async (req, res, next) => {
      const token = createSecretToken(user._id);
      res.cookie("token", token, {
        withCredentials: true,
-       httpOnly: false,
-        secure: true,
-        sameSite: "none"
+       httpOnly: true,
+       secure: true,
+       sameSite: "none"
      });
      res.status(201).json({ message: "User logged in successfully", success: true });
      next()
